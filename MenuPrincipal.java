810af7b5abc0857e11ae5b8df07fab7e3b348701
Billy Torres <btorresr75@gmail.com>
2023-07-03 09:29:40 -0600

    Menu principal finalizado

--- MenuPrincipal.java
+++ MenuPrincipal.java
@@ -1,9 +1,15 @@
+import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
 public class MenuPrincipal {
 
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Hola Mundo");
+        JOptionPane.showMessageDialog(null, "Seleccione una opción de conversión");
+        Object[] opciones = { "Conversor de moneda", "Conversor de Temperatura" };
+
+        String opcionSelecionada = (String) JOptionPane.showInputDialog(null, "Seleccione una opcion de conversor",
+                "Menu", JOptionPane.PLAIN_MESSAGE, null, opciones,
+                opciones[0]);
     }
 
 }
