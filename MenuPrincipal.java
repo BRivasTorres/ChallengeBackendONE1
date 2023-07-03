6c085dd2856766d239b7fd94daaa560518ca4c77
Billy Torres <btorresr75@gmail.com>
2023-07-03 09:06:48 -0600

    Instalando la libreria JOptionPane

---
+++ MenuPrincipal.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class MenuPrincipal {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hola Mundo");
+    }
+
+}
