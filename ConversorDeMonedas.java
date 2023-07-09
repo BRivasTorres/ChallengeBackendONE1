696dd2f7744c8770166bdd3bcbdc5178118385b1
Billy Torres <btorresr75@gmail.com>
2023-07-09 09:22:19 -0600

    Cuadro de dialogo entrada valor concluido

--- ConversorDeMonedas.java
+++ ConversorDeMonedas.java
@@ -2,12 +2,13 @@ import javax.swing.JOptionPane;
 
 public class ConversorDeMonedas {
 
-    public static void main(String[] args) {
+    public static void Convertir() {
+        double tasaCambio = 7.85;
         String cantidadStr;
         int cantidad = 0;
         Boolean esNumero = false;
         do {
-            cantidadStr = JOptionPane.showInputDialog(null, "Ingrese una cantidad numerica");
+            cantidadStr = JOptionPane.showInputDialog(null, "Ingrese la cantidad de dinero que deseas convertir");
             if (cantidadStr.matches("\\d+")) {
                 cantidad = Integer.parseInt(cantidadStr);
                 esNumero = true;
@@ -15,6 +16,8 @@ public class ConversorDeMonedas {
                 JOptionPane.showMessageDialog(null, "Deve ingresar solamente numeros");
             }
         } while (!esNumero);
-        JOptionPane.showMessageDialog(null, "La cantidad ingresada es: " + cantidad);
+        double resultadoCantidad = tasaCambio * cantidad;
+        JOptionPane.showMessageDialog(null, "La cantidad ingresada es: " + resultadoCantidad);
     }
+
 }
--- MenuPrincipal.java
+++ MenuPrincipal.java
@@ -3,12 +3,15 @@ import javax.swing.JOptionPane;
 public class MenuPrincipal {
 
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Seleccione una opción de conversión");
         Object[] opciones = { "Conversor de moneda", "Conversor de Temperatura" };
 
         String opcionSelecionada = (String) JOptionPane.showInputDialog(null, "Seleccione una opcion de conversor",
                 "Menu", JOptionPane.PLAIN_MESSAGE, null, opciones,
                 opciones[0]);
+        if (opcionSelecionada.equals("Conversor de moneda")) {
+            ConversorDeMonedas conversorDeMonedas = new ConversorDeMonedas();
+            ConversorDeMonedas.Convertir();
+        }
     }
 
 }
