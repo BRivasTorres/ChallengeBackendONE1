e55477c82577163ff2b916d6fc3f4c4662425f6f
Billy Torres <btorresr75@gmail.com>
2023-07-11 16:35:10 -0600

    Borrando viejo array de conversiones

--- OpcionesDeMoneda.java
+++ OpcionesDeMoneda.java
@@ -1,14 +1,39 @@
+import java.util.Map;
+import java.util.HashMap;
 import javax.swing.JOptionPane;
+import javax.swing.RowFilter.Entry;
 
 public class OpcionesDeMoneda {
-    public static void Conversor(int cantidad, String tipo) {
-        // JOptionPane.showInputDialog()
+    public static void main(String[] args) {
+        Map<String, Double> cambios = new HashMap<>();
+        cambios.put("Dolares", .13);
+        cambios.put("Euro", .12);
+        cambios.put("Libras Esterlinas", .10);
+        cambios.put("Yen Japones", 18.27);
+        cambios.put("Won", 166.93);
 
-        // double dolares = .13;
-        // double euro = .12;
-        // double librasEsterlinas = .10;
-        // double yenJapones = 18.27;
-        // double won = 166.93;
+        String monedas[] = {};
 
+        // for (Map.Entry<String, Double> entry : cambios.entrySet()) {
+        // monedas[] = { entry.getKey() };
+        // }
+
+        // JOptionPane.showInputDialog(null, "Elija moneda", "Elija moneda", 0, null,
+        // monedas, monedas[0]);
     }
+
+    // public static void Conversor(int cantidad, String tipo) {
+
+    // Map<String, Double> cambios = new HashMap<>();
+    // cambios.put("Dolares", .13);
+    // cambios.put("Euro", .12);
+    // cambios.put("Libras Esterlinas", .10);
+    // cambios.put("Yen Japones", 18.27);
+    // cambios.put("Won", 166.93);
+
+    // for (Map.Entry<String, Double> entry : cambios.entrySet()) {
+    // System.out.println(entry.getKey());
+    // }
+
+    // }
 }
